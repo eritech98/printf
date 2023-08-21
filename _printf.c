ade0e8ae14d74d1bbe480831f777901573d6b05b
Erick Olando <opiyoerick1998@gmail.com>
2023-08-21 13:35:37 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int e, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -31,11 +31,11 @@ int _printf(const char *format, ...)
 
 /* va_list with the formart*/
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (e = 0; format && format[e] != '\0'; e++)
 	{
-		if (format[i] != '%')
+		if (format[e] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[e];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
@@ -44,12 +44,12 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
+			flags = get_flags(format, &e);
+			width = get_width(format, &e, list);
+			precision = get_precision(format, &e, list);
+			size = get_size(format, &e);
+			++e;
+			printed = handle_print(format, &e, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
